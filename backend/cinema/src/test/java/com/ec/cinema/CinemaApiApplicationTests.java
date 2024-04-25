dd9faa8c3b34545de5be2528646b7bbc9330bd91
Brian <diazbrian47@gmail.com>
2024-04-25 16:27:19 -0500

    first commit

---
+++ backend/cinema/src/test/java/com/ec/cinema/CinemaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ec.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
